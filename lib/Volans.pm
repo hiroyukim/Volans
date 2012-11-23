efde566eddca01767cbc84f301bfe1d6a133f098
yamanaka <yamanaka@fashion-co-lab.jp>
2012-11-23 14:51:01 +0900

    init commit

---
+++ lib/Volans.pm
@@ -0,0 +1,32 @@
+package Volans;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Volans -
+
+=head1 SYNOPSIS
+
+  use Volans;
+
+=head1 DESCRIPTION
+
+Volans is
+
+=head1 AUTHOR
+
+hiroyuki yamanaka E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
